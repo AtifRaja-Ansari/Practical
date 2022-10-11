7006f52ee51874c1fcd8fac8e00a01b29f750ed3
AtifRaja-Ansari <111556392+AtifRaja-Ansari@users.noreply.github.com>
2022-10-11 23:55:05 +0530

    Add files via upload

---
+++ 11th oct/FinalClass.java
@@ -0,0 +1,16 @@
+
+//Final Keyword----
+//Final class-- cannot extend the final class
+package demo;
+
+final class FinalClass{}
+class test extends FinalClass{//can't extend final class
+	void show()
+	{
+		System.out.println("Nothing");
+		}
+	public static void main(String[] args) {
+		test t= new test();
+				t.show();
+	}
+}
