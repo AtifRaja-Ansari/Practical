88f5968870d05e0d07b4018e56c6a24d8e6de7e0
AtifRaja-Ansari <111556392+AtifRaja-Ansari@users.noreply.github.com>
2022-12-14 23:50:53 +0530

    Add files via upload

---
+++ 13-Dec-2022/HashSetEg.java
@@ -0,0 +1,22 @@
+package collection;
+import java.util.*;
+public class HashSetEg {
+	
+	public static void main(String[] args) {
+		Set<String> hs = new HashSet<String>(); // creating hash set
+		hs.add("Ac"); // duplicate value not allowed
+		hs.add("pencil");
+		hs.add("Mouse");
+		hs.add("Phone");
+		hs.add(null);
+		hs.add("Ac");
+		hs.add("Bottle");
+		hs.add("Earphone");
+		hs.add("Pen");
+		Iterator<String> itr = hs.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+	}
+}
